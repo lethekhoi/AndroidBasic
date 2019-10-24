ef86ae789c68b55961807ec57202e9482b70f079
lethekhoi <lethekhoi@gmail.com>
2019-10-24 21:10:24 +0700

    feat: bai tap

--- app/src/main/java/lethekhoi/ltk/hcmus/androidbassic/MainActivity.java
+++ app/src/main/java/lethekhoi/ltk/hcmus/androidbassic/MainActivity.java
@@ -11,22 +11,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        inSoNguyenTo();
+        // Math method
+        //static : mặc định đã có ko cần tạo lại
+        // 1 : làm tròn : Math.round()
+        float number1 = 0.4f;
+        number1 = Math.round(number1);
+        Log.d("AAA", number1 + "");
+//        // 2 : làm tròn lên : 2.1 ->3
+//        float number2 = 2.1f;
+//
+//        number2 = (float) Math.ceil(number2);
+//        Log.d("BBB", number2 + "");
+//        // 3 : làm tròn xuống : 2.9->2
+//        float number3 = 2.9f;
+//        number3 = (float) Math.floor(number3);
+//
+        //4 : Math.random
+//random sao cho các số có tỉ lệ đều nhau (cộng cho 1 rồi mới nhân sau đó lấy floor xuống )
+//        for (int i = 0; i <= 100; i++) {
+//            double number4 = Math.flooLog.d("CCC", number3 + "");r(Math.random() * 6);
+//            Log.d("CCC", number4 + "");
+// 5 tìm ra số lớn nhát so nhỏ nhất   Log.d("CCC", number3 + "");
+        int[] a = {1, 10, 11, 20, 18, 19, 21, 99};//phan khai bao va khoi tao
+
+//        int max = a[0];
+//        for (int value : a) {
+//
+//            max = Math.max(max, value);
+//        }
+//        Log.d("CCC", max + "");
+//
+//
+//        int min = a[0];
+//        for (int value : a) {
+//
+//            min = Math.min(min, value);
+//        }
+//        Log.d("CCC", min + "");
+        //6 : sắp xếp
+
+
     }
 
-    private void inSoNguyenTo() {
-
-
-        for (int i = 0; i <= 100; i++) {
-            int flag = 0;
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0) {
-                    flag = flag + 1;
-                }
-
-            }
-            if (flag == 2) Log.d("BBB", i + "");
-        }
-
-    }
 }
